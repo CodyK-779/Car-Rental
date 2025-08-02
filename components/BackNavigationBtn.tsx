3048fd23f4d653321ff6b75aba45dd5b42bee6dd
CodyK-779 <CodyK9627@example.com>
2025-08-02 14:11:30 +0630

    Finish project

---
+++ components/BackNavigationBtn.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import { ArrowLeftIcon } from "lucide-react";
+
+const BackNavigationBtn = () => {
+  const router = useRouter();
+
+  return (
+    <Button
+      asChild
+      onClick={() => router.back()}
+      className="mb-8 mt-16 flex items-center gap-2"
+    >
+      <ArrowLeftIcon />
+      Back to Cars
+    </Button>
+  );
+};
+
+export default BackNavigationBtn;
